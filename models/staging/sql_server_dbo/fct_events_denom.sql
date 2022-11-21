cd1e931a9d9f76840bd37145c8466d6cf5936f5c
samuel.bs87@gmail.com <samuel.bs87@gmail.com>
2022-11-21 19:02:18 +0000

    Inicio planteamiento 21/11/2022

--- models/staging/sql_server_dbo/fct_events_denom.sql
+++ models/staging/sql_server_dbo/fct_events_denom.sql
@@ -1,17 +1,17 @@
 {{ config(materialized="table") }}
 
-with
-    fct_events as (select * from {{ source("sql_server_dbo", "events") }})
+with fct_events as (select * from {{ source("sql_server_dbo", "events") }})
+    
 
-    select fe.event_id
-        ,fe.page_url
-        ,fe.event_type
-        ,fe.product_id
-        ,fe.session_id
-        ,fe.created_at as event_created_at
-        ,fe.order_id
-        , a.*
+select
+    fe.event_id,
+    fe.page_url,
+    fe.event_type,
+    fe.product_id,
+    fe.session_id,
+    fe.created_at as event_created_at,
+    fe.order_id,
+    a.*
 
-    from fct_events fe
-    left join {{ ref("dim_users_addresses") }} a
-    on fe.user_id = a.user_id
+from fct_events fe
+left join {{ ref("dim_users_addresses") }} a on fe.user_id = a.user_id
