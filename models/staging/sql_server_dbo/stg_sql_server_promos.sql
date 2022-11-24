4d50f14c2f1ab069f5cd472ecbb4c7e7bc6a97f7
samuel.bs87@gmail.com <samuel.bs87@gmail.com>
2022-11-24 17:06:45 +0000

    WIP 24/11 -  Crear STG

---
+++ models/staging/sql_server_dbo/stg_sql_server_promos.sql
@@ -0,0 +1,16 @@
+with
+    stg_promos as (select * from {{ source("sql_server_dbo", "promos") }}),
+
+    renamed_casted as (
+        select
+           promo_id,
+           status,
+           discount,
+           _fivetran_deleted,
+           _fivetran_synced
+            
+        from stg_promos
+    )
+select *
+from renamed_casted 
+
